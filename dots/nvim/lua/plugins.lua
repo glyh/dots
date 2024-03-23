2018441aa89ae794a69193d601b03fe2753cdfe7
glyh <lyhokia@gmail.com>
2024-03-23 22:12:33 +0800

    ranger in neovim

--- dots/nvim/lua/plugins.lua
+++ dots/nvim/lua/plugins.lua
@@ -95,7 +95,35 @@ return {
   { 'nvim-tree/nvim-tree.lua',
     cmd = {'NvimTreeFocus', 'NvimTreeClose'},
   },
-
+  { 'kelly-lin/ranger.nvim',
+    lazy = false,
+    config = function()
+      local ranger_nvim = require('ranger-nvim')
+      ranger_nvim.setup({
+        enable_cmds = false,
+        replace_netrw = true,
+        keybinds = {
+          ['ov'] = ranger_nvim.OPEN_MODE.vsplit,
+          ['oh'] = ranger_nvim.OPEN_MODE.split,
+          ['ot'] = ranger_nvim.OPEN_MODE.tabedit,
+          ['or'] = ranger_nvim.OPEN_MODE.rifle,
+        },
+        ui = {
+          border = 'none',
+          height = 1,
+          width = 1,
+          x = 0.5,
+          y = 0.5,
+        }
+      })
+      vim.api.nvim_set_keymap('n', '<leader>ef', '', {
+        noremap = true,
+        callback = function()
+          require('ranger-nvim').open(true)
+        end,
+      })
+    end,
+  },
   { 'mfussenegger/nvim-dap',
     config = function() require 'configs.dap' end,
   },
