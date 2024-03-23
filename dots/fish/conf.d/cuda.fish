1607e864924384be7c1728688a9a5dbb371c8db9
glyh <lyhokia@gmail.com>
2024-03-24 07:11:44 +0800

    update

--- dots/fish/conf.d/cuda.fish
+++ dots/fish/conf.d/cuda.fish
@@ -3,4 +3,5 @@ if not test -d /opt/cuda
 end
 
 set -gxa PATH /opt/cuda/bin
-set -gx NVCC_PREPEND_FLAGS '-ccbin /opt/cuda/bin/g++'
+set -gx NVCC_PREPEND_FLAGS "-ccbin /opt/cuda/bin/g++"
+set -gx CUDA_CACHE_PATH $XDG_CACHE_HOME"/nv"
