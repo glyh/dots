d28bb513ce7619d9ec0e557813d1a1e41b8d2da4
glyh <lyhokia@gmail.com>
2024-03-28 13:36:58 +0800

    update

--- dots/nvim/lua/plugins.lua
+++ dots/nvim/lua/plugins.lua
@@ -83,15 +83,11 @@ return {
           -- Python {{{
           "pyright",
           -- }}}
-          -- Fsharp {{{
-          "fsautocomplete",
-          -- }}}
         },
 
         auto_update = false,
         run_on_start = true,
         start_delay = 3000,
-        debounce_hours = 5,
       }
     end,
   },
